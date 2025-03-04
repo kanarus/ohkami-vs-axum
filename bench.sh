881220d6c9dfea12e14ae2831c6e8b8f0b4ab77e
kanarus <kanarus786@gmail.com>
2025-03-05 04:23:06 +0900

    add log functionality

--- bench.sh
+++ bench.sh
@@ -5,11 +5,6 @@ set -Cue -o pipefail
 function run_wrk () {
     path="$1"
 
-    if [ $path = "" ]; then
-        echo "expected argument is not passed to 'run_wrk'"
-        exit 1
-    fi  
-
     wrk \
         -H 'Accept: */*' \
         -H 'Connection: keep-alive' \
@@ -21,6 +16,36 @@ function run_wrk () {
 }
 
 function run_benchmark () {
+    framework="$1"
+    comment="$2"
+
+    wd="$PWD"
+
+    echo "Starting benchmark..."
+
+    echo "For manual cleanup, run:
+
+        kill \$(ps aux | awk '/target\\/release/ {print \$2}') && docker container stop 'postgres'
+    "
+
+    docker run -d --rm \
+        -p 5432:5432 \
+        -e POSTGRES_USER=benchmarkdbuser \
+        -e POSTGRES_PASSWORD=benchmarkdbpass \
+        -e POSTGRES_DB=hello_world \
+        -v $wd/postgres:/docker-entrypoint-initdb.d \
+        --name postgres \
+        postgres:17-bookworm
+
+    sleep 3s
+
+    cd ./$framework && \
+    (./run.sh &) && \
+    cd $wd
+
+    echo "framework '$framework' ($comment) is running"
+    
+    result=''
     paths=(
         '/json'
         '/db'
@@ -33,35 +58,25 @@ function run_benchmark () {
         '/updates?q=1024'
         '/plaintext'
     )
-
-    target_framework="$1"
-
-    if [ $target_framework = "" ]; then
-        echo "Please pass a framework name !"
-        exit 1
-    fi
-
-    echo "Starting benchmark..."
-
-    docker run -d --rm \
-        -p 5432:5432 \
-        -e POSTGRES_USER=benchmarkdbuser \
-        -e POSTGRES_PASSWORD=benchmarkdbpass \
-        -e POSTGRES_DB=hello_world \
-        -v $(pwd)/postgres:/docker-entrypoint-initdb.d \
-        --name postgres \
-        postgres:17-bookworm
-
-    sleep 3s
-
-    cd ./$target_framework && (./run.sh &)
-
-    echo "Server of framework '$target_framework' is running"
-
     for path in "${paths[@]}"; do
-        sleep 1m
-        run_wrk "$path"
+        echo "preparing benchmark for '$path'..."
+
+        sleep 30s
+
+        rps=$(run_wrk "$path" | awk '/^Requests\/sec/ {print $2}')
+        echo "$rps reqs/sec for '$path'"
+        if [ "$result" != '' ]; then
+            result="$result,"
+        fi
+        result="$result\"$path\": $rps"
     done
+    result="{$result}"
+
+    timestamp=$(date -u +'%Y%m%d%H%M%S')
+    log_jsonc="./.log/$framework-$timestamp.jsonc"
+    echo "/* $comment */" >  $log_jsonc
+    echo ""               >> $log_jsonc
+    echo $result | jq     >> $log_jsonc
 
     echo "Finishing benchmark..."
 }
@@ -74,4 +89,10 @@ function cleanup() {
     echo "Done !"
 }
 
-run_benchmark "$1"; cleanup
+
+if [ $# != 2 ]; then
+    echo 'usage: ./bench.sh <framework> <comment (what you changed for it)>'
+    exit 1
+fi
+run_benchmark "$1" "$2"
+cleanup
